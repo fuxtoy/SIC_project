514742e4c655deb0ee67c741d53bc6eb4378a8f1
fuxtoy <timchen0719@gmail.com>
2023-03-31 22:17:11 +0800

    v3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react';
+import {Pass1} from './Pass1.jsx';
+import {Pass2} from './Pass2.jsx';
+export default function App() {
 
-function App() {
+  const [content, setContent] = useState('');
+  const [proglen, setProglen] = useState('');
+  const handleFileRead = (e) => {
+    const content = e.target.result;
+    setContent(content);
+    document.getElementById("output3").value = content
+    console.log('file content',  content)
+  };
+  const handleFileChosen = (file) => {
+    const fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
+  function writeoutput1(){
+    var result_pass1 = Pass1(content);
+    console.log(result_pass1.SYMTAB)
+    setProglen(result_pass1.proglen)
+    document.getElementById("output1").value = JSON.stringify(result_pass1.SYMTAB)
+  }
+  function writeoutput2(){
+    const SYMTAB = document.getElementById("output1").value
+    document.getElementById("output2").value = JSON.stringify(Pass2(content,SYMTAB,proglen))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <div class = "flex-container">
+        <div class = "flex-column">
+          <input 
+            id="input" type="file" accept=".asm" 
+            onChange={(e) => handleFileChosen(e.target.files[0])}>
+          </input>
+          <button class = "button" onClick={writeoutput1}
+            >PASS1</button>
+          <button class = "button" onClick={writeoutput2}
+            >PASS2</button>
+          <input id="output3" type="text" disabled={true} class ="output3" ></input>
+        </div>
 
-export default App;
+        <input id="output1" type="text" disabled={true} class = "textbox"></input>
+        <input id="output2" type="text" disabled={true} class = "textbox"></input>
+      </div>
+
+    </div>
+  )
+}
